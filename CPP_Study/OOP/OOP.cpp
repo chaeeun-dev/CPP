333bd5f1e7fee3c08e60e09b3b76d41583242452
im-chaeeun <ce0118@tukorea.ac.kr>
2024-07-16 13:23:06 +0900

    class
    생성자, 소멸자

--- CPP_Study/OOP/OOP.cpp
+++ CPP_Study/OOP/OOP.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Player.h"
 
 // 객체지향 (Object Oriented Programming)
 // 데이터 + 로직
@@ -9,16 +8,61 @@
 // 객체? 플레이어, 몬스터, GameRoom, Lobby
 
 // 설계도
+class Knight
+{
+public:
+	// 기본 생성자 (constructor)
+	//Knight()	// 초기값 설정 중요
+	//{
+	//	_hp = 0;
+	//	_attack = 0;
+	//	_defence = 0;
+
+	//	std::cout << "Knight()" << std::endl;
+	//}
+	
+	// 복사 생성자 
+	Knight(const Knight& other)
+	{
+		this->_hp = other._hp;
+		this->_attack = other._attack;
+		this->_defence = other._defence;
+		std::cout << "Knight(const Knight& other)" << std::endl;
+	}
+
+	// 기타 생성자 - 변수 값을 다 채워야할 때
+	Knight(int hp, int attack, int defence)
+	{
+		this->_hp = hp;
+		this->_attack = attack;
+		this->_defence = defence;
+		std::cout << "Knight()" << std::endl;
+	}
+	// 소멸자
+	~Knight()
+	{
+		std::cout << "~knight()" << std::endl;
+	}
+	// 멤버 함수
+	void Attack() { std::cout << "ATTACK" << std::endl; }
+	void Die() { std::cout << "DIE" << std::endl; }
+	void HealMe(int Value) {
+		_hp += 10;
+	}
+
+public:
+	// 멤버 변수
+	int _hp;
+	int _attack;
+	int _defence;
+};
 
 
 int main()
 {
 	// 객체 (Instance)
-	Player k1;
+	Knight k1(100, 10, 1);
 
-	k1._hp = 100;
-	k1._attack = 10;
-
-	k1.Attack();
-	k1.Die();
+	Knight k2(k1);		// 복사 생성자
+	std::cout << k1._hp << std::endl;
 }
