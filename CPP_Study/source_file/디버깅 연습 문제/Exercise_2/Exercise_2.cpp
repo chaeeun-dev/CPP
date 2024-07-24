9fc9987fc73155ce933beff6cf77c57d37bd531d
im-chaeeun <dev.chaeng@gmail.com>
2024-07-24 21:27:13 +0900

    문제2 풂

--- CPP_Study/source_file/디버깅 연습 문제/Exercise_2/Exercise_2.cpp
+++ CPP_Study/source_file/디버깅 연습 문제/Exercise_2/Exercise_2.cpp
@@ -8,6 +8,12 @@ using namespace std;
 // 그런데 정보를 출력하다 프로그램이 뻗는 문제가 발생하고 있습니다.
 // 크래시 원인을 분석하고 알맞게 고쳐주세요!
 
+// 내가 해결한 방법
+// PrintInfo()에서 오류가 발생했는데,
+// for (int i = 0; i <= KNIGHT_COUNT; i++)에서 <=로 인해
+// 배열의 크기 이상으로 메모리에 접근했기 때문에 엑세스 위반이 발생했다는 걸 발견했다.
+// 따라서 for ( int i = 0; i < KNIGHT_COUNT; i++)로 코드를 수정했다.
+
 const int KNIGHT_COUNT = 10;
 
 int main()
@@ -19,7 +25,7 @@ int main()
 		knights[i] = new Knight();
 	}
 
-	for (int i = 0; i <= KNIGHT_COUNT; i++)
+	for (int i = 0; i < KNIGHT_COUNT; i++)
 	{
 		knights[i]->PrintInfo();
 		delete knights[i];
