a7b1dd2d26c561946f828443047074d4b6a3d977
Chaeeun0118 <ce0118ce@gmail.com>
2024-07-26 21:36:47 +0900

    양방향 연결리스트 구현 중...

--- CPP_Study/source_file/LinearStructure/Maze/Maze/List.h
+++ CPP_Study/source_file/LinearStructure/Maze/Maze/List.h
@@ -1,5 +1,51 @@
 #pragma once
+
+class Node
+{
+	// typedef int T;	// 옛날 문법
+	using T = int;
+public:
+	Node(int data) : data(data), prev(nullptr), next(nullptr) { }
+public:
+	T data;
+	Node* prev;
+	Node* next;
+};
+
 class List
 {
+public:
+	List()
+	{
+
+	}
+
+	~List()
+	{
+
+	}
+
+	Node* AddAtHead(int data)
+	{
+		Node* node = new Node(data);
+
+		if (_head == nullptr)
+		{
+			_head = node;
+			_tail = node;
+		}
+		else
+		{
+			// 꼭 그림 그리면서 코드 작성하기!
+			Node* next_node = _head;
+			node->next = next_node;
+			next_node->prev = node;
+			_head = node;
+		}
+	}
+
+private:
+	Node* _head = nullptr;
+	Node* _tail = nullptr;
 };
 
