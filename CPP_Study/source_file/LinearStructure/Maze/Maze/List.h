b8dce2bbb69d8db73dcfa013124038dc90a41b01
Chaeeun0118 <ce0118ce@gmail.com>
2024-07-26 23:03:09 +0900

    dummy 노드를 사용해서 head와 tail에 노드 추가하는 코드 작성

--- CPP_Study/source_file/LinearStructure/Maze/Maze/List.h
+++ CPP_Study/source_file/LinearStructure/Maze/Maze/List.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 class Node
 {
@@ -17,31 +18,76 @@ class List
 public:
 	List()
 	{
-
+		_head = new Node(0);	// 여기서 data는 중요하지 않음
+		_tail = new Node(0);
+		_head->next = _tail;
+		_tail->prev = _head;
 	}
 
-	~List()
+	~List()	// 노드 지워주는 부분
 	{
-
+		Node* node = _head;
+		while (node)
+		{
+			Node* deleteNode = node;
+			node = node->next;	// 노드를 한 칸씩 이동
+			delete deleteNode;
+		}
 	}
 
+	void Print()
+	{
+		Node* node = _head->next;
+		while (node != _tail)
+		{
+			std::cout << node->data << std::endl;
+			node = node->next;
+		}
+	}
+
+	//	[dummy] <->[1]<->[2]<->[3]<-> [dummy]
+	//  [head]						   [tail]
 	Node* AddAtHead(int data)
 	{
-		Node* node = new Node(data);
+		// dummy 노드가 없을 때
+		//Node* node = new Node(data);
+		//if (_head == nullptr)
+		//{
+		//	_head = node;
+		//	_tail = node;
+		//}
+		//else
+		//{
+		//	// 꼭 그림 그리면서 코드 작성하기!
+		//	Node* next_node = _head;
+		//	node->next = next_node;
+		//	next_node->prev = node;
+		//	_head = node;
+		//}
 
-		if (_head == nullptr)
-		{
-			_head = node;
-			_tail = node;
-		}
-		else
-		{
-			// 꼭 그림 그리면서 코드 작성하기!
-			Node* next_node = _head;
-			node->next = next_node;
-			next_node->prev = node;
-			_head = node;
-		}
+		// dummy 노드 사용 - if else문을 안 써도 돼서 깔끔함
+		Node* node = new Node(data);
+		Node* nextNode = _head->next;
+
+		node->next = nextNode;
+		nextNode->prev = node;
+		_head->prev = node;
+		node->next = _head;
+
+		return node;
+	}
+
+	Node* AddAtTail(int data)
+	{
+		Node* node = new Node(data);
+		Node* prevNode = _tail->prev;
+
+		prevNode->next = node;
+		node->prev = prevNode;
+		node->next = _tail;
+		_tail->prev = node;
+
+		return node;
 	}
 
 private:
--- CPP_Study/source_file/LinearStructure/Maze/Maze/Maze.cpp
+++ CPP_Study/source_file/LinearStructure/Maze/Maze/Maze.cpp
@@ -1,9 +1,13 @@
-﻿// Maze.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include "List.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    List li;
+    
+    li.AddAtTail(1);
+    li.AddAtTail(2);
+    li.AddAtTail(3);
+
+    li.Print();
 }
